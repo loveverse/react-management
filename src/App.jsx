22373c8685cb34f39ae3bf4953dae2fadc06beca
love <loveverse@qq.com>
2021-11-03 21:27:11 +0800

    login

---
+++ src/App.jsx
@@ -0,0 +1,27 @@
+import { Component } from 'react';
+// import { message,Button } from 'antd'
+import { BrowserRouter, HashRouter, Route, Switch } from 'react-router-dom';
+
+import Login from './pages/login/login'
+import Admin from './pages/admin/admin'
+
+export default class App extends Component {
+  render() {
+    return (
+      // <div>
+      //   <Button type="primary" onClick={this.handleClick}>Primary Button</Button>
+      // </div>
+
+      <HashRouter>
+        {/* 只匹配其中一个 */}
+        <Switch>
+          <Route path='/login' component={Login}></Route>
+          <Route path='/' component={Admin}></Route>
+        </Switch>
+      </HashRouter>
+    )
+  }
+  // handleClick = () => {
+  //   message.success('This is a success message');
+  // }
+}
